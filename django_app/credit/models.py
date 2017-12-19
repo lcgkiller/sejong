532815b95484b86f28ec339fe485f1cf8a774683
lcgkiller <lcgkiller@gmail.com>
2017-12-19 14:00:35 +0900

    db modelling

--- django_app/credit/models.py
+++ django_app/credit/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Credit(models.Model):
+    admission_year = models.IntegerField()              # 입학년도
+    college = models.CharField(max_length=30)           # 단과대학
+    department = models.CharField(max_length=30)        # 학과명
+
+    core_essential = models.IntegerField()              # 중핵필수
+    core_essential_selection = models.IntegerField()    # 중핵필수선택
+
+    major_basic = models.IntegerField()                 # 전공기초교양
+    sum_of_major = models.IntegerField()                # 전공학점 합계
+    major_required = models.IntegerField()              # 전공필수
+    major_select = models.IntegerField()                # 전공선택
+
+    sum_of_all = models.IntegerField()                  # 졸업학점
