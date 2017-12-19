81b85258f7d518cff45fc4f16f680c23f6ba8b86
lcgkiller <lcgkiller@gmail.com>
2017-12-19 11:24:46 +0900

    first commit

---
+++ django_app/config/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'config.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
